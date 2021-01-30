6aebe7562537e4ca2100385e4a8d9a28cc1403a9
Cameron Bacon <cameronmbacon@gmail.com>
2021-01-30 02:34:48 -0800

    views test

--- scanner/urls.py
+++ scanner/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('scannerapp', include('scannerapp.urls')),
+    path('scannerapp/', include('scannerapp.urls')),
     path('admin/', admin.site.urls),
 ]

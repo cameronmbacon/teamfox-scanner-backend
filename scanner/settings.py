665ec2fbf8eacf37c4023050b75dfefa308bd3fe
Cameron Bacon <cameronmbacon@gmail.com>
2021-01-30 03:10:53 -0800

    imported scapy module

--- scanner/settings.py
+++ scanner/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['teamfox-scanner-backend-stage.herokuapp.com']
+ALLOWED_HOSTS = [
+    'teamfox-scanner-backend-stage.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
--- scannerapp/views.py
+++ scannerapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import scapy.all as scapy
+import socket
 
 # Create your views here.
 def first(request):
-    return HttpResponse('Response test.')
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    return HttpResponse('IP Address = ' + IPAddr)
